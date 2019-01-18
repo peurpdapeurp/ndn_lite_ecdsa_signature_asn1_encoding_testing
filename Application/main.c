89d1ffd867b0b181b3d10e5d6761c34ec13ca40e
Edward Lu <edwardzlu98@gmail.com>
2019-01-18 01:05:21 -0800

    Cleaned up main.c

--- Application/main.c
+++ Application/main.c
@@ -3,18 +3,8 @@
 #include "app-init-files/app_initialization_functions.h"
 
 #include "print-helper.h"
-
 #include "basic-asn-encode-decode-tests/basic-asn-encode-decode-tests.h"
 #include "ecdsa-sig-sign-verify-tests/ecdsa-sig-sign-verify-tests.h"
-
-#include "../../ndn-lite/security/detail/default-backend/sec-lib/micro-ecc/uECC.h"
-#include "../../ndn-lite/security/ndn-lite-sec-config.h"
-#include "../../ndn-lite/security/ndn-lite-rng.h"
-#include "../../ndn-lite/adaptation/ndn-nrf-ble-adaptation/nrf-logger.h"
-#include "../../ndn-lite/ndn-enums.h"
-
-uint8_t test_hack_pub_ecc[100];
-uint8_t test_hack_prv_ecc[100];
 
 /**@brief Function for application main entry.
  */
@@ -24,19 +14,6 @@ int main(void) {
   log_init();
 
   printf("Entered main function of main_board1.c\n");
-
-  //**//
-  ndn_security_init();
-
-  uECC_set_rng(ndn_rng);
-
-  uECC_Curve curve = uECC_secp256k1();
-  int pub_key_size = uECC_curve_public_key_size(curve);
-  int pri_key_size = uECC_curve_private_key_size(curve);
-  uECC_make_key(test_hack_pub_ecc, test_hack_prv_ecc, curve);
-
-  NRF_APP_LOG_HEX("Public key generated (test hack secp256k1):", test_hack_pub_ecc, pub_key_size);
-  NRF_APP_LOG_HEX("Private key generated (test hack secp256k1):", test_hack_prv_ecc, pri_key_size);
 
   // Initialize timers.
   timers_init();
@@ -48,10 +25,10 @@ int main(void) {
     run_ecdsa_sig_sign_verify_tests()
   )
   {
-    NRF_APP_LOG("ALL TESTS PASSED.\n");
+    APP_LOG("ALL SIGN-VERIFY TESTS PASSED.\n");
   }
   else {
-    NRF_APP_LOG("ONE OR MORE TESTS FAILED.\n");
+    APP_LOG("ONE OR MORE SIGN-VERIFY TESTS FAILED.\n");
   }
 
 }
--- Output/ndn_lite_asn_testing Debug/Obj/main.d
+++ Output/ndn_lite_asn_testing Debug/Obj/main.d
@@ -96,19 +96,4 @@ Output/ndn_lite_asn_testing\ Debug/Obj/main.o: \
  C:\Users\edwar\Desktop\ndn-lite-work\ndn-lite-asn-testing\Application\print-helper.h \
  C:\Users\edwar\Desktop\ndn-lite-work\ndn-lite-asn-testing\Application\basic-asn-encode-decode-tests/basic-asn-encode-decode-tests.h \
  Application/../../ndn-lite/ndn-constants.h \
- C:\Users\edwar\Desktop\ndn-lite-work\ndn-lite-asn-testing\Application\ecdsa-sig-sign-verify-tests/ecdsa-sig-sign-verify-tests.h \
- c:\users\edwar\desktop\ndn-lite-work\ndn-lite\security\detail\default-backend\sec-lib\micro-ecc\uecc.h \
- c:\users\edwar\desktop\ndn-lite-work\ndn-lite\security\ndn-lite-sec-config.h \
- c:\users\edwar\desktop\ndn-lite-work\ndn-lite\security\detail/default-backend/ndn-lite-default-sha-impl.h \
- c:\users\edwar\desktop\ndn-lite-work\ndn-lite\security\detail/nordic-sdk-nrf-backend/ndn-lite-nrf-crypto-rng-impl.h \
- c:\users\edwar\desktop\ndn-lite-work\ndn-lite\security\detail/default-backend/ndn-lite-default-aes-impl.h \
- c:\users\edwar\desktop\ndn-lite-work\ndn-lite\security\detail/default-backend/ndn-lite-default-ecc-impl.h \
- c:\users\edwar\desktop\ndn-lite-work\ndn-lite\security\detail/default-backend/sec-lib/tinycrypt/tc_ecc.h \
- c:\users\edwar\desktop\ndn-lite-work\ndn-lite\security\detail/default-backend/sec-lib/micro-ecc/uECC.h \
- c:\users\edwar\desktop\ndn-lite-work\ndn-lite\ndn-constants.h \
- c:\users\edwar\desktop\ndn-lite-work\ndn-lite\security\detail/default-backend/ndn-lite-default-hmac-impl.h \
- c:\users\edwar\desktop\ndn-lite-work\ndn-lite\security\ndn-lite-rng.h \
- c:\users\edwar\desktop\ndn-lite-work\ndn-lite\ndn-error-code.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.10/include/inttypes.h \
- c:\users\edwar\desktop\ndn-lite-work\ndn-lite\adaptation\ndn-nrf-ble-adaptation\nrf-logger.h \
- c:\users\edwar\desktop\ndn-lite-work\ndn-lite\ndn-enums.h
+ C:\Users\edwar\Desktop\ndn-lite-work\ndn-lite-asn-testing\Application\ecdsa-sig-sign-verify-tests/ecdsa-sig-sign-verify-tests.h
