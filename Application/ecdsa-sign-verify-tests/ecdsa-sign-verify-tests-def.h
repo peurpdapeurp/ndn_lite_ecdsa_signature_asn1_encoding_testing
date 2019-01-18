82b0cc3c4f2cdd4c29170216fede1bcd3bb08211
Edward Lu <edwardzlu98@gmail.com>
2019-01-18 01:47:35 -0800

    Extra cleanup, added copyright things

--- Application/ecdsa-sign-verify-tests/ecdsa-sign-verify-tests-def.h
+++ Application/ecdsa-sign-verify-tests/ecdsa-sign-verify-tests-def.h
@@ -1,4 +1,14 @@
 
+/*
+ * Copyright (C) Edward Lu
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ *
+ * See AUTHORS.md for complete list of NDN IOT PKG authors and contributors.
+ */
+
 #ifndef ECDSA_SIGN_VERIFY_TESTS_DEF_H
 #define ECDSA_SIGN_VERIFY_TESTS_DEF_H
 
--- Application/sha256-sign-verify-tests/sha256-sign-verify-tests-def.c
+++ Application/sha256-sign-verify-tests/sha256-sign-verify-tests-def.c
@@ -1,4 +1,14 @@
 
+/*
+ * Copyright (C) Edward Lu
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ *
+ * See AUTHORS.md for complete list of NDN IOT PKG authors and contributors.
+ */
+
 #include "sha256-sign-verify-tests-def.h"
 
 char *sha256_sign_verify_test_names[SHA256_SIGN_VERIFY_NUM_TESTS] = {
